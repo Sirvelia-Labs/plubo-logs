5c1936d203ba74a5b59d414176f48d0135e3c2b3
Albert-Sirvelia <albert@sirvelia.com>
2024-05-03 18:08:11 +0200

    Changed vendor path constant

--- PluboLogs/AdminMenu.php
+++ PluboLogs/AdminMenu.php
@@ -29,9 +29,9 @@ class AdminMenu
     public static function enqueue_log_scripts( $hook )
     {
         if ( $hook === self::$PLUBO_TOOLS_PAGE && !wp_script_is( self::$PLUBO_HANDLE_NAME ) ) {
-            $assets_url = PLUBO_LOGS_ASSETS_URL;
-            if ( defined( 'PLUBO_LOGS_VENDOR_PATH' ) ) {
-                $assets_url = PLUBO_LOGS_VENDOR_PATH;
+            $assets_url = plugin_dir_url( __FILE__ ) . '../dist/';
+            if ( defined( 'PLUBO_LOGS_ASSETS_PATH' ) ) {
+                $assets_url = PLUBO_LOGS_ASSETS_PATH;
             }
 
             wp_enqueue_script( self::$PLUBO_HANDLE_NAME, $assets_url . 'app.js', [], self::$PLUBO_ADMIN_VERSION );
